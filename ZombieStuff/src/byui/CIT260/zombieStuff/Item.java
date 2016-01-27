320967bd15fe588eb8f9df37ed02c55dbae67c58
t4d3 <t4d3sliving@gmail.com>
2016-01-27 12:20:04 -0700

    yeah buddy

---
+++ ZombieStuff/src/byui/CIT260/zombieStuff/Item.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.zombieStuff;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author T4d3-T550
+ */
+public class Item {
+    private String itemDescription;
+    private int isWieldable;
+
+}
