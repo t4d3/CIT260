30a0faf84a2a69789e6e4d68d22243ff60f65d1a
jpcox <coxjustinp@gmail.com>
2016-01-28 12:12:43 -0700

    1-28-2016 Coompleted GameCharacter Class

--- ZombieStuff/src/byui/CIT260/zombieStuff/model/GameCharacter.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/model/GameCharacter.java
@@ -5,10 +5,99 @@
  */
 package byui.CIT260.zombieStuff.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
  * @author Justin Cox
  */
-public class GameCharacter {
+public class GameCharacter implements Serializable {
+    private String name;
+    private String description;
+    private double attack;
+    private double strength;
+
+    public GameCharacter() {
+    }
+
     
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAttack() {
+        return attack;
+    }
+
+    public void setAttack(double attack) {
+        this.attack = attack;
+    }
+
+    public double getStrength() {
+        return strength;
+    }
+
+    public void setStrength(double strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.name);
+        hash = 47 * hash + Objects.hashCode(this.description);
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.attack) ^ (Double.doubleToLongBits(this.attack) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.strength) ^ (Double.doubleToLongBits(this.strength) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "GameCharacter{" + "name=" + name + ", description=" + description + ", attack=" + attack + ", strength=" + strength + '}';
+    }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GameCharacter other = (GameCharacter) obj;
+        if (Double.doubleToLongBits(this.attack) != Double.doubleToLongBits(other.attack)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.strength) != Double.doubleToLongBits(other.strength)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+    
+
 }
