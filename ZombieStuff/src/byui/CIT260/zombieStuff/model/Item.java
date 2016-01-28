e52913da9e6db39fdf2470408959db443fc3a69e
jpcox <coxjustinp@gmail.com>
2016-01-28 11:55:44 -0700

    1-28-2016 Added Character class and changed .gitignore file

--- ZombieStuff/src/byui/CIT260/zombieStuff/model/Item.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/model/Item.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byui.CIT260.zombieStuff;
+package byui.CIT260.zombieStuff.model;
 
 import java.io.Serializable;
 
