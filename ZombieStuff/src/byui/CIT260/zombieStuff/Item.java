c1dc292c0be182505edcab18f433e31c9f59ca6d
t4d3 <t4d3sliving@gmail.com>
2016-01-28 11:41:15 -0700

    Thursday, 1/28 lots of changes to the itme class

--- ZombieStuff/src/byui/CIT260/zombieStuff/Item.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/Item.java
@@ -15,11 +15,36 @@ public class Item implements Serializable{
     private String itemDescription;
     private int isWieldable;
 
-    public Item(String itemDescription) {
-        this.itemDescription = itemDescription;
+    public Item() {
     }
 
-    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + this.isWieldable;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "itemDescription=" + itemDescription + ", isWieldable=" + isWieldable + '}';
+    }
+        
     
     public String getItemDescription() {
         return itemDescription;
