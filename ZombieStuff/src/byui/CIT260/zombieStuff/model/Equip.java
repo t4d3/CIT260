4e038ec8b561b7a3cd9d443641b561e2f8ad67f0
t4d3 <t4d3sliving@gmail.com>
2016-01-28 22:00:09 -0700

    Thursday, 1/28 added Equip class, and added ItemID to the Item class.

---
+++ ZombieStuff/src/byui/CIT260/zombieStuff/model/Equip.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.zombieStuff.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author T4d3-T550
+ */
+public class Equip {
+    private Integer AttributeDefence;
+    private Integer AttributeAttack;
+    private Integer ObjectID;
+
+    public Integer getAttributeDefence() {
+        return AttributeDefence;
+    }
+
+    public void setAttributeDefence(Integer AttributeDefence) {
+        this.AttributeDefence = AttributeDefence;
+    }
+
+    public Integer getAttributeAttack() {
+        return AttributeAttack;
+    }
+
+    public void setAttributeAttack(Integer AttributeAttack) {
+        this.AttributeAttack = AttributeAttack;
+    }
+
+    public Integer getObjectID() {
+        return ObjectID;
+    }
+
+    public void setObjectID(Integer ObjectID) {
+        this.ObjectID = ObjectID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.ObjectID);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Equip other = (Equip) obj;
+        if (!Objects.equals(this.AttributeDefence, other.AttributeDefence)) {
+            return false;
+        }
+        if (!Objects.equals(this.AttributeAttack, other.AttributeAttack)) {
+            return false;
+        }
+        return Objects.equals(this.ObjectID, other.ObjectID);
+    }
+
+    @Override
+    public String toString() {
+        return "Equip{" + "AttributeDefence=" + AttributeDefence + ", AttributeAttack=" + AttributeAttack + ", ObjectID=" + ObjectID + '}';
+    }
+    
+    
+}
