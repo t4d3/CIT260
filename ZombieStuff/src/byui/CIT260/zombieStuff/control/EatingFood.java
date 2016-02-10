5c9cc5785400c2f05cab5702a927415bd7f331c0
t4d3 <t4d3sliving@gmail.com>
2016-02-10 07:52:04 -0700

    Wednesday 2/10: Added the EatingFood class

---
+++ ZombieStuff/src/byui/CIT260/zombieStuff/control/EatingFood.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.zombieStuff.control;
+
+/**
+ *
+ * @author T4d3-T550
+ */
+public class EatingFood {
+    public int eatFood(int healthCurrent, int numCornDogs, float money) {
+        if (healthCurrent >= 10) {
+            return healthCurrent;
+            // ERROR: they have no need for food
+        }
+        else if (0 == healthCurrent) {
+            return -1;
+            //ERROR: they are dead
+        }
+        else if (numCornDogs > (10 - healthCurrent)) {
+            return -2;
+            //ERROR: too many cornDogs
+        }
+        else if (numCornDogs < (10 - healthCurrent)) {
+            //WARNING: you could have more cornDogs
+            //CONTINUE onto the purchase
+            ;
+        }
+        else if (numCornDogs == (10 - healthCurrent)) {
+            //CONTINUE onto the purchase
+            ;
+        }
+// the purchase portion of the code
+        double maxCostOfCornDogs = (1.75 * numCornDogs)*.22 + (1.75 * numCornDogs);
+        double minCostOfCornDogs = (1.75 * numCornDogs)*.12 + (1.75 * numCornDogs);
+        if (money > maxCostOfCornDogs) {
+            return -1;
+            //ERROR: that tip is bogus!
+        }
+        if (money < minCostOfCornDogs) {
+            return -1;
+            //ERROR: where's the tip?
+        }
+	healthCurrent += numCornDogs;
+	return healthCurrent;
+    }
+}
