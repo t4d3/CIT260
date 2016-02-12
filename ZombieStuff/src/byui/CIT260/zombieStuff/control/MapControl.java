2e54cedd741e7ff479644e0163e532cf8c0fb624
jpcox <coxjustinp@gmail.com>
2016-02-12 11:36:57 -0700

    2-9-2016 Completed Map,Location, and Game controls.

---
+++ ZombieStuff/src/byui/CIT260/zombieStuff/control/MapControl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.zombieStuff.control;
+
+/**
+ *
+ * @author Justin Cox
+ */
+public class MapControl {
+    
+    public static double(String currentLocation, String locationChoice, double totalTime, double timeUsed)
+    {
+        if(timeLeft > totalTime)
+        {
+            return -1;
+        }
+        timeRemaining = totalTime - (timeUsed + timeToLocation);
+        
+            return timeRemaining;
+    }
+}
