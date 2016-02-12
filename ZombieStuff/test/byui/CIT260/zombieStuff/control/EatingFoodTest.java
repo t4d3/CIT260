8df86e934f3a7c83372ff50b04431e604de5ca91
t4d3 <t4d3sliving@gmail.com>
2016-02-12 12:00:31 -0700

    Wednesday 2/12: Tested the EatingFood thing-a-majig.

---
+++ ZombieStuff/test/byui/CIT260/zombieStuff/control/EatingFoodTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.zombieStuff.control;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author T4d3-T550
+ */
+public class EatingFoodTest {
+    
+    public EatingFoodTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of eatFood method, of class EatingFood.
+     */
+    @Test
+    public void testEatFood() {
+        System.out.println("eatFood");
+        int healthCurrent = 5;
+        int numCornDogs = 5;
+        float money = 10.23F;
+        EatingFood instance = new EatingFood();
+        int expResult = 10;
+        int result = instance.eatFood(healthCurrent, numCornDogs, money);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+    
+}
