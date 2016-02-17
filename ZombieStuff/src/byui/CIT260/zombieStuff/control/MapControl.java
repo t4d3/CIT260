5b748b6ab7050102d18fed4713510bfc6627c5a9
t4d3 <t4d3sliving@gmail.com>
2016-02-17 12:25:46 -0700

    Wednesday 2/12: screw it

--- ZombieStuff/src/byui/CIT260/zombieStuff/control/MapControl.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/control/MapControl.java
@@ -10,15 +10,5 @@ package byui.CIT260.zombieStuff.control;
  * @author Justin Cox
  */
 public class MapControl {
-    
-    public static double(String currentLocation, String locationChoice, double totalTime, double timeUsed)
-    {
-        if(timeLeft > totalTime)
-        {
-            return -1;
-        }
-        timeRemaining = totalTime - (timeUsed + timeToLocation);
-        
-            return timeRemaining;
-    }
+
 }
