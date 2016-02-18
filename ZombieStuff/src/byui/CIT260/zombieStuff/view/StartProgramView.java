db28bcbd90702690adc7668d63dee121510d88fd
t4d3 <t4d3sliving@gmail.com>
2016-02-18 12:32:32 -0700

    Wednesday 2/18: got rid of map control

--- ZombieStuff/src/byui/CIT260/zombieStuff/view/StartProgramView.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/view/StartProgramView.java
@@ -5,6 +5,10 @@
  */
 package byui.CIT260.zombieStuff.view;
 
+import byui.CIT260.zombieStuff.control.GameControl;
+import byui.CIT260.zombieStuff.model.Player;
+import java.util.Scanner;
+
 /**
  *
  * @author Justin Cox
@@ -14,9 +18,10 @@ public class StartProgramView {
     private String promptMessage;
     
     public StartProgramView(){
+        this.displayBanner();
         this.promptMessage ="\nPlease enter your name: ";
+        System.out.println(promptMessage);
     //Display the banner screen
-    this.displayBanner();
  //Get the players name
  //Create a new player
  //DISPLAY a customized welcome message
@@ -40,5 +45,58 @@ public class StartProgramView {
 
     public void displayStartProgramView() {
         System.out.println("\n***displayProgramView function called ***");
+        boolean done = false;
+        do {
+            String playerName = this.getPlayerName();
+            if (playerName.toUpperCase().equals("Q"))
+                return;
+            done = this.doAction(playerName);
+        } while (!done);
+    }
+
+    private String getPlayerName() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        System.out.println("\n*** getPlayerName() Call***");
+        boolean valid;
+        valid = false;
+        while(!valid) {// a valid value has not been entered
+            System.out.println("\n" + this.promptMessage);
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length() < 1) {
+                System.out.println("Invalid enytry.  value cannot be blank!");
+                continue;
+            }
+            
+            break;
+        }
+        return value;
+    }
+
+    private boolean doAction(String playerName) {
+        System.out.println("\n*** doAction() Call***");
+        if (playerName.length() < 2) {
+            System.out.println("Invalid entry...must be longer than one letter.");
+            return false;
+        }
+        
+        Player player = GameControl.createPlayer(playerName);
+        
+        if (player == null) {
+            System.out.println("Error: cannot create player");
+            return false;
+        }
+        this.displayNextView();
+        return false;
+    }
+    
+    
+    
+    //end of program
+
+    private void displayNextView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
