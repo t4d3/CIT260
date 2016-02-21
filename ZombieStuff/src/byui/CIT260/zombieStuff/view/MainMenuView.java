230ff0ef3f968e030bc679145449e8c863b9bb9e
t4d3 <t4d3sliving@gmail.com>
2016-02-20 21:19:23 -0700

    Wednesday 2/19: Almost done with team ass. 6

--- ZombieStuff/src/byui/CIT260/zombieStuff/view/MainMenuView.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/view/MainMenuView.java
@@ -5,24 +5,29 @@
  */
 package byui.CIT260.zombieStuff.view;
 
+import byui.CIT260.zombieStuff.control.GameControl;
+import java.util.Scanner;
+import zombiestuff.ZombieStuff;
+
 /**
  *
  * @author T4d3-T550
  */
 public class MainMenuView {
     private String menu;
-    public void MainMenueView() {
-        this.menu = "\n*************************************"
-                  + "\n         Main Menu"
-                  + "N - New game"
-                  + "R - Retrieve and resume old game"
-                  + "H - Display a Help menu"
-                  + "S - Save game"
-                  + "Q - Quit";
+    public void MainMenuView() {
+        System.out.println( "\n*************Main Menu***************"
+                          + "\nN - New game"
+                          + "\nR - Retrieve and resume old game"
+                          + "\nH - Display a Help menu"
+                          + "\nS - Save game"
+                          + "\nQ - Quit"
+                          + "\n*************************************" );
     }
     public void displayMainMenuView() {
         boolean done = false;
         do {
+            this.MainMenuView();
             String menuOption = this.getMenuOption();
             if (menuOption.toUpperCase().equals("Q"))
                 return;
@@ -32,12 +37,82 @@ public class MainMenuView {
 
     private String getMenuOption() {
         System.out.println("\n*** getMenuOption() called***");
-        return "N";
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid;
+        valid = false;
+        do {
+            value = keyboard.nextLine();
+            value = value.trim();
+            if (value.length() != 1) {
+                System.out.println("ERROR: Invalid entry; must be one letter");
+            }
+            else valid = true;
+        } while (!valid);
+        return value;
     }
 
     private boolean doAction(String menuOption) {
         System.out.println("\n*** doAction() called ***");
-        return true;
+        menuOption = menuOption.toUpperCase();
+
+        switch (menuOption) {
+            case "N" :
+                    this.startNewGame();
+                    break;
+            case "R":
+                    this.startExistingGame();
+                    break;
+            case "H":
+                    this.displayHelpMenu();
+                    break;
+            case "S":
+                    this.saveGame();
+                    break;
+            case "Q":
+                    this.quitGame();
+                    break;
+            default:
+                System.out.println("\nERROR: Invalid selection... Try again!");
+                break;
+            
+        }
+        return false;
+    }
+
+    private void startNewGame() {
+        System.out.println("*** startNewGame() called ***");
+        GameControl.createNewGame(ZombieStuff.getPlayer());
+        System.out.println("*** startNewGame() called again ***");
+
+        GameMenuView gameView = new GameMenuView();
+        gameView.displayMenu();
+//Create a new Game
+//Create a new Game Menu View
+//Display the Game Menu 
+    }
+
+    private void startExistingGame() {
+        System.out.println("\n*** startExistingGame() called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("\n*** displayHelpMenu() called ***");
+        System.out.println( "\n*************Main Menu***************"
+                + "\nG - What is the goal of the game?"
+                + "\nM - How to move" 
+                + "\nE - Estimating the amount of resources"
+                + "\nH - Harvesting resources"
+                + "\nD - Delivering resources to warehouse"
+                + "\nQ - Quit");
+                }
+
+    private void saveGame() {
+        System.out.println("\n*** saveGame() called ***");
+    }
+
+    private void quitGame() {
+        System.out.println("\n*** quitGame() called ***");
     }
 }
 
