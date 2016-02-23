15a894c67b68bd3594246e843e8729d896c3d2cd
t4d3 <t4d3sliving@gmail.com>
2016-02-23 07:30:40 -0700

    Monday, 2\22, Added in the MainMenuView stuff

--- ZombieStuff/src/byui/CIT260/zombieStuff/view/GameMenuView.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/view/GameMenuView.java
@@ -5,16 +5,76 @@
  */
 package byui.CIT260.zombieStuff.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author T4d3-T550
  */
 class GameMenuView {
     public void displayMenu() {
-        System.out.println("\n*** displayMenu() called ***");
-//tmp function to see the map in the program.
+        boolean done = false;
+        do {
+            this.GameMenuView();
+            String menuOption = this.getMenuOption();
+            if (menuOption.toUpperCase().equals("Q"))
+                return;
+            done = this.doAction(menuOption);
+        } while (!done);
+    }
+
+    private GameMenuView() {
+        System.out.println("****************Game menu******************"
+                + "* G - Go to a store                       *"
+                + "* M - Display the Map                     *"
+                + "*******************************************");
+    }
+
+    private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid;
+        valid = false;
+        do {
+            value = keyboard.nextLine();
+            value = value.trim();
+            if (value.length() != 1)
+                System.out.println("ERROR: Invalid entry; must be one letter");
+            else valid = true;
+        } while (!valid);
+        return value;
+    }
+    private boolean doAction(String menuOption) {
+        System.out.println("\n*** doAction() called ***");
+        menuOption = menuOption.toUpperCase();
+
+        switch (menuOption) {
+            case "M" :
+                    this.MapView();
+                    break;
+                    /*            case "R":
+                    this.startExistingGame();
+                    break;
+                    case "H":
+                    this.displayHelpMenu();
+                    break;
+                    case "S":
+                    this.saveGame();
+                    break;
+                    case "Q":
+                    this.quitGame();
+            break;*/
+            default:
+                System.out.println("\nERROR: Invalid selection... Try again!");
+                break;
+            
+        }
+        return false;
+    }
+
+    private void MapView() {
         MapView mapView = new MapView();
         mapView.MapView();
     }
-    
+
 }
