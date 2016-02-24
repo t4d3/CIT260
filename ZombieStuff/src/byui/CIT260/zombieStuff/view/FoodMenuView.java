cafed466e14d8cbcb9d9571e12b013769c32eb0f
jpcox <coxjustinp@gmail.com>
2016-02-24 11:09:53 -0700

    2-24-2016 Created the FoodMenuView class

---
+++ ZombieStuff/src/byui/CIT260/zombieStuff/view/FoodMenuView.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.CIT260.zombieStuff.view;
+
+/**
+ *
+ * @author Justin Cox
+ */
+public class FoodMenuView {
+    public void FoodMenuView(){
+        System.out.println(
+               "*            FEELING HUNGRY ARE YOU?          *"
+            +  "*\n                                           *"
+            +  "*\n         What would you like to eat?       *"
+            +  "*\n                                           *"
+            +  "*\n         Choice #1: Corn Dog               *"
+            +  "*\n                                           *"
+            +  "*\n                                           *"
+            +  "*\n         Choice #2: Corn Dog               *"
+            +  "*\n                                           *"
+            +  "*\n                                           *"
+            +  "*\n         Choice #3: Corn Dog               *");
+    }
+}
