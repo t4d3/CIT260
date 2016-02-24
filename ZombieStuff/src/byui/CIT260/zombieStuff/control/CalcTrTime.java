60fa70a3159ca63179330727cb2cce32afcf3d2d
t4d3 <t4d3sliving@gmail.com>
2016-02-24 13:15:18 -0700

    Monday, 2\22, Brought back CalcTravelTime

---
+++ ZombieStuff/src/byui/CIT260/zombieStuff/control/CalcTrTime.java
@@ -0,0 +1,33 @@
+/*
+ * This code should only return the time between two points, so that the calling
+ *  function can either warn the player of the travel time, or assign the travel
+ *  time to the timeUsed.  Either return the time used, or false?
+ *  If the return is a positive number, then the location change worked!
+ */
+package byui.CIT260.zombieStuff.control;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author T4d3-T550
+ */
+public class CalcTravelTime {
+    public int calcTravelTime(Point ALocation, Point BLocation, int timeUsed, int maxTime) {
+        if (timeUsed >= maxTime ) {
+            return -2;
+            //ERROR: You are out of time!
+        }
+        else if (timeUsed < 0) {
+            return -1;
+            //ERROR: you cannot be negative time!
+        }
+        double distSqr = (Math.pow((ALocation.getX() - BLocation.getX()), 2)
+                         + Math.pow((ALocation.getY() - BLocation.getY()), 2));
+        int timeToTravel = (int) Math.sqrt(distSqr); //This is the total Distance (travel time)
+        if ((timeToTravel + timeUsed) > maxTime) {
+            return -3;
+        }
+        return timeToTravel;
+   }
+}
