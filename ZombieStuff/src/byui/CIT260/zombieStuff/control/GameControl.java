153f71769798e1bb7e3b9a8fd4e273811a6b2bd0
t4d3 <t4d3sliving@gmail.com>
2016-03-24 12:37:51 -0600

    3/24 more stuff

--- ZombieStuff/src/byui/CIT260/zombieStuff/control/GameControl.java
+++ ZombieStuff/src/byui/CIT260/zombieStuff/control/GameControl.java
@@ -6,7 +6,6 @@
 package byui.CIT260.zombieStuff.control;
 
 import byui.CIT260.zombieStuff.exceptions.GameControlException;
-import byui.CIT260.zombieStuff.exceptions.MapControlException;
 import byui.CIT260.zombieStuff.model.Game;
 import byui.CIT260.zombieStuff.model.Item;
 import byui.CIT260.zombieStuff.model.Map;
@@ -34,24 +33,28 @@ public class GameControl {
         return player;
     }
 
-    public static void createNewGame(Player player)
-            throws MapControlException {
-        Game game = new Game();
-        ZombieStuff.setCurrentGame(game);
-        game.setPlayer(player);
-        
-        Item[] inventoryList = GameControl.createInventory();
-        game.setInventory(inventoryList);
-        
-        Map map = MapControl.createMap();
-        game.setMap(map);
-        
-        MapControl.moveCharacterToStartingLocation(map);
-    }
+    public static void createNewGame(Player player) throws GameControlException {
+        try {
+            Game game = new Game();
 
-//    public static updateLocation() {
-//        call calcTravelTime
-//            if there is no error, the update the location
+            ZombieStuff.setCurrentGame(game);
+            game.setPlayer(player);
+
+            Item[] inventoryList = GameControl.createInventory();
+            game.setInventory(inventoryList);
+
+            Map map = MapControl.createMap();
+            game.setMap(map);
+
+            MapControl.moveCharacterToStartingLocation(map);
+        } catch (Exception e) {
+            throw new GameControlException(e.getMessage());
+        }
+    }
+    //    public static updateLocation() {
+    //        call calcTravelTime
+    //            if there is no error, the update the location
+
     public static void updateGameTime(int desiredTravelTime) {
         System.out.println("***updateGameTime() Called***");
         ZombieStuff.getCurrentGame().setTotalTime(ZombieStuff.getCurrentGame().getTotalTime() + desiredTravelTime);
@@ -60,16 +63,17 @@ public class GameControl {
     public static void updateLocation(Point desiredLocation)
             throws GameControlException {
         System.out.println("***UpdateLocation() Called***");
-        
+
         //calculate the travel time to that store;
         //if the travel time is negative, RETURN Error Code
         //Update character's location
         //update total time  try movePlayerView > updateLocation > calcTravelTime > updateGameTime throw
         //return total time
-        if (false)
+        if (false) {
             throw new GameControlException("Cannot Update Location"
-                + desiredLocation + ", because it's dumb. "
-                + "CurrentLocation is unchanged");
+                    + desiredLocation + ", because it's dumb. "
+                    + "CurrentLocation is unchanged");
+        }
     }
 
     private static Item[] createInventory() {
@@ -77,15 +81,14 @@ public class GameControl {
         Item[] item = {};
         return item;
     }
-    
+
     public static void saveGame(String filePath) throws GameControlException {
-                try (FileOutputStream fops = new FileOutputStream(filePath)) {
+        try (FileOutputStream fops = new FileOutputStream(filePath)) {
             ObjectOutputStream output = new ObjectOutputStream(fops);
 
             output.writeObject(ZombieStuff.getCurrentGame());
         } catch (Exception e) {
             throw new GameControlException(e.getMessage());
         }
-
     }
 }
