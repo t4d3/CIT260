4a65a7eec3c6e94425ec7a5bf9dd8aca79297b9b
t4d3 <t4d3sliving@gmail.com>
2016-03-24 21:14:54 -0600

    3/24 finished the Team ass. 11

--- ZombieStuff/src/zombiestuff/ZombieStuff.java
+++ ZombieStuff/src/zombiestuff/ZombieStuff.java
@@ -12,21 +12,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 /**
  *
  * @author T4d3-T550
  */
 public class ZombieStuff {
+
     private static Game currentGame = null;
     private static Player player = null;
-    
+
     private static PrintWriter outFile = null;
     private static BufferedReader inFile = null;
-    
+
     private static PrintWriter logFile = null;
 
     public static PrintWriter getOutFile() {
@@ -68,43 +66,45 @@ public class ZombieStuff {
     public static void setPlayer(Player player) {
         ZombieStuff.player = player;
     }
-    
+
     public static void main(String[] args) {
-        
+
         try {
 
-        ZombieStuff.inFile =
-                new BufferedReader(new InputStreamReader(System.in));
-        
-        ZombieStuff.outFile = new PrintWriter(System.out, true);
-        
-        String filePath = "C:\\Users\\Public\\Downloads\\log.txt";
-        ZombieStuff.logFile = new PrintWriter(filePath);
-        
-        //display the banner
-        StartProgramView startProgramView = new StartProgramView();
-        startProgramView.display();
-        
-    } catch (Throwable e) {
-        System.out.println("Exception: " + e.toString() +
-                "\nCause: " + e.getCause() +
-                "\nMessage: " + e.getMessage());
-        
-        e.printStackTrace();
-    }
-        finally {
+            ZombieStuff.inFile
+                    = new BufferedReader(new InputStreamReader(System.in));
+
+            ZombieStuff.outFile = new PrintWriter(System.out, true);
+
+            String filePath = "C:\\Share\\log.txt";
+            ZombieStuff.logFile = new PrintWriter(filePath);
+
+            //display the banner
+            StartProgramView startProgramView = new StartProgramView();
+            startProgramView.display();
+
+        } catch (Throwable e) {
+            System.out.println("Exception: " + e.toString()
+                    + "\nCause: " + e.getCause()
+                    + "\nMessage: " + e.getMessage());
+
+            e.printStackTrace();
+        } finally {
             try {
-                if (ZombieStuff.inFile != null)
+                if (ZombieStuff.inFile != null) {
                     ZombieStuff.inFile.close();
-                if (ZombieStuff.logFile != null)
+                }
+                if (ZombieStuff.logFile != null) {
                     ZombieStuff.logFile.close();
-                if (ZombieStuff.outFile != null)
+                }
+                if (ZombieStuff.outFile != null) {
                     ZombieStuff.outFile.close();
+                }
             } catch (IOException ex) {
                 System.out.println("Error Closing Files");
                 return;
             }
         }
     }
-    
+
 }
